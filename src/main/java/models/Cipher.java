0ff73459e77b7ac1f208ca2e67bd65ff67016442
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-06 23:11:19 +0300

    declaring the values and assigning them to their data types

--- src/main/java/models/Cipher.java
+++ src/main/java/models/Cipher.java
@@ -8,7 +8,10 @@ public class Cipher {
         pText = pText.toLowerCase();
         StringBuilder cText = new StringBuilder();
         for (int i = 0; i<pText.length(); i++){
-
+            int charIndex = alphabet.indexOf(pText.charAt(i));
+            int newIndex = (charIndex + key) % 26;
+            char cipherChar = alphabet.charAt(newIndex);
+            cText.append(cipherChar);
         }
     }
     }
