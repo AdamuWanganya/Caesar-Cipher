862fc731b773e1e8b496bcec125e52367b1266c2
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-06 23:31:40 +0300

    Ask the user to enter the text using System.out.println() method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,13 @@
+import models.Cipher;
+
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        Cipher testCipher = new Cipher();
+        System.out.println("Enter plain Text");
+        String plain = scan.nextLine();
     }
     }
