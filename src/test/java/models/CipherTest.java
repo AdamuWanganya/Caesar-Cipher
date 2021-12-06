d4a661bcdf96ea80cc0e68eb869f753fa1032467
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-06 23:50:01 +0300

    create and declare encoding method

--- src/test/java/models/CipherTest.java
+++ src/test/java/models/CipherTest.java
@@ -8,6 +8,22 @@ class CipherTest {
 
     @Test
     void encoding() {
+
+        Cipher testCipher = new Cipher();
+        String pText = "welcome to coding";
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int key = 3;
+        pText = pText.toLowerCase();
+        StringBuilder cText = new StringBuilder();
+        for (int i = 0; i<pText.length(); i++){
+            int charIndex = alphabet.indexOf(pText.charAt(i));
+            int newIndex = (charIndex + key) % 26;
+            char cipherChar = alphabet.charAt(newIndex);
+            cText.append(cipherChar);
+        }
+
+        assertEquals(cText,pText);
     }
 
     @Test
