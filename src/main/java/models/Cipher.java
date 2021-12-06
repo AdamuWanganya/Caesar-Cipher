992412165cc8b23c48016cd9e867afbbd552e04e
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-06 23:08:26 +0300

    converte plain text to lower case using .toLowerCase() method

--- src/main/java/models/Cipher.java
+++ src/main/java/models/Cipher.java
@@ -5,7 +5,7 @@ public class Cipher {
     String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     public String encoding(String pText, int key) {
-
+        pText = pText.toLowerCase();
     }
     }
 }
