18e4e095f0cdf6a2763a12e380a991fe235b07c7
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-06 23:46:44 +0300

    generate methods tests

---
+++ src/test/java/models/CipherTest.java
@@ -0,0 +1,16 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CipherTest {
+
+    @Test
+    void encoding() {
+    }
+
+    @Test
+    void decoding() {
+    }
+}
